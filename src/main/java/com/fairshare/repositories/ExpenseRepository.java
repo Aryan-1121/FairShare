1ddf2539a5dd2841fe45303ced74fee384a1aafc
Aryan <aryanmaurya879@gmail.com>
2024-06-08 18:27:14 +0530

    repo annotation

--- src/main/java/com/fairshare/repositories/ExpenseRepository.java
+++ src/main/java/com/fairshare/repositories/ExpenseRepository.java
@@ -2,6 +2,8 @@ package com.fairshare.repositories;
 
 import com.fairshare.models.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 }
