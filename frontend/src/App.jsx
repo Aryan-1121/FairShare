eae5870b4ea9a20dd73cbf2fc1254a8bf32bf8e4
Aryan <aryanmaurya879@gmail.com>
2024-06-09 20:46:40 +0530

    routers added in app jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
+import AddExpense from './components/AddExpense';
+import SettleUp from './components/SettleUp';
 
 function App() {
-
   return (
-    <div>
-      "Hello World"
-    </div>
-
-  )
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/add-expense" component={AddExpense} />
+        <Route path="/settle-up" component={SettleUp} />
+      </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
