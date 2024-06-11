33222057ff392798b4a68034cf9a3843e4827a42
Aryan <aryanmaurya879@gmail.com>
2024-06-12 03:49:27 +0530

    added password encoder and authenticationManager bean in security config file

--- backend/src/main/java/com/fairshare/config/WebSecurityConfig.java
+++ backend/src/main/java/com/fairshare/config/WebSecurityConfig.java
@@ -5,15 +5,21 @@ import com.fairshare.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.Map;
+import java.util.Set;
 import java.util.logging.Filter;
 
 @Configuration
@@ -28,6 +34,24 @@ public class WebSecurityConfig  {
     private JwtRequestFilter jwtRequestFilter;
 
 //private Filter filter;
+
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+
+
+
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
         http.csrf(AbstractHttpConfigurer :: disable)
