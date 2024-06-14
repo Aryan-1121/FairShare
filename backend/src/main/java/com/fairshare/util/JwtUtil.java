299cc720a995a360dba0f45f1c53c9e82af172fc
Aryan <aryanmaurya879@gmail.com>
2024-06-14 18:14:22 +0530

    added functionality to get payload from token and verify if its valid or not with given userName and expiration check

--- backend/src/main/java/com/fairshare/util/JwtUtil.java
+++ backend/src/main/java/com/fairshare/util/JwtUtil.java
@@ -1,15 +1,24 @@
 package com.fairshare.util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 public class JwtUtil {
 
     private String jwtSecret;
 
+
+
+    public String generateToken(String userName){
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userName);
+    }
 
 //this  method will take payload (subject) and will sigh it using the jwtSecret and will set an expiration time of the generated token(string)
 //    subject is the payload(userName) which is going to be signed by the jwtSecret
@@ -26,9 +35,43 @@ public class JwtUtil {
 
 //    this will decode the token and extract whatever the payload was (username in this case)
     public String extractUserName(String token ){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+//    similarly we will decode and get the expiration time/date
+    public Date extractExpirationDateTime(String token ){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+
+//    now we also need to make a function which check if the token is expired or not
+    private Boolean isTokenExpired(String token ){
+        return extractExpirationDateTime(token).before(new Date());
+    }
+
+
+
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+
+    private Claims extractAllClaims(String token){
+         return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
 
     }
 
+
+
+
+
+
+    public Boolean validateToken(String token, String userName){
+        final String extractedUserName = extractUserName(token);
+        return (extractedUserName.equals(userName) && !isTokenExpired(token));
+    }
 
 
 
