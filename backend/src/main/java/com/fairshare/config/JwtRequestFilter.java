67f426e36129cff30934e7a1bd84e3a374f4c006
Aryan <aryanmaurya879@gmail.com>
2024-06-15 17:29:27 +0530

    added method functionality to extract token from header and then userName from that token

--- backend/src/main/java/com/fairshare/config/JwtRequestFilter.java
+++ backend/src/main/java/com/fairshare/config/JwtRequestFilter.java
@@ -1,9 +1,12 @@
 package com.fairshare.config;
 
+import com.fairshare.services.UserService;
+import com.fairshare.util.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,12 +16,33 @@ import java.io.IOException;
 public class JwtRequestFilter extends OncePerRequestFilter {
 
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
 
 
 
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        //1st need to get the Authorization header from the request
+        final String authorizationHeader = request.getHeader("Authorization");
+
+        String signedJwt = null;
+        String extractedUserName= null;
+//        if the Authorization header is present & starts with "Bearer" then extract the jwt token
+
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+            signedJwt = authorizationHeader.substring(7);       //"Bearer " -> length = 7 after this main token starts
+
+//      decoding the token will give us username and expiration date/time
+            extractedUserName = jwtUtil.extractUserName(signedJwt);
+        }
+
 
     }
 }
