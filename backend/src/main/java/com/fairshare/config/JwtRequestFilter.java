d9776c01bc623676f6695a1fb74c3fdeadade728
Aryan <aryanmaurya879@gmail.com>
2024-06-17 04:21:25 +0530

    Authentication at its peak :)

--- backend/src/main/java/com/fairshare/config/JwtRequestFilter.java
+++ backend/src/main/java/com/fairshare/config/JwtRequestFilter.java
@@ -1,5 +1,6 @@
 package com.fairshare.config;
 
+import com.fairshare.services.UserDetailsServiceImpl;
 import com.fairshare.services.UserService;
 import com.fairshare.util.JwtUtil;
 import jakarta.servlet.FilterChain;
@@ -7,6 +8,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,7 +22,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
 
     @Autowired
-    private UserService userService;
+    private UserDetailsServiceImpl userDetailsService;
 
     @Autowired
     private JwtUtil jwtUtil;
@@ -46,6 +51,29 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
         // If the username is extracted and no authentication is set in the context, proceed to authenticate
 
+//  TODO : THIS IS TAKING USERNAME AS AN ARGUMENT BUT, LOADBYUSERNAME METHOD INTERNALLY IS CALLING FINDBYEMAIL, NEED TO CHANGE ITS INTERNAL CALLING
+
+//        Checks if the username is not null and the security context is not already authenticated.
+        if(extractedUserName != null && SecurityContextHolder.getContext().getAuthentication()== null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUserName(extractedUserName); // getting user details
+
+//after getting userDetails validated the token
+            if(jwtUtil.validateToken(signedJwt, userDetails.getUsername())){
+                // if token is valid then -> Create a UsernamePasswordAuthenticationToken with the user details and authorities
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                // Setting additional details for the authentication token
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+//  Setting above authentication token in the SecurityContext
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+
+
+        }
+
+//        if not authenticated, then authenticate
+        filterChain.doFilter(request, response);
 
 
     }
