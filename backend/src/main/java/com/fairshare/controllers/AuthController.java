73ed766ad706bb33a2158cd3931c13dabddd7e2e
Aryan <aryanmaurya879@gmail.com>
2024-06-18 09:28:30 +0530

    added authetication req/res classes where req - accepts usrnam+ pass and res- returns jwt

--- backend/src/main/java/com/fairshare/controllers/AuthController.java
+++ backend/src/main/java/com/fairshare/controllers/AuthController.java
@@ -1,5 +1,15 @@
 package com.fairshare.controllers;
 
+import com.fairshare.models.User;
+import com.fairshare.services.UserDetailsService;
+import com.fairshare.services.UserService;
+import com.fairshare.util.JwtUtil;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,10 +17,63 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserService userService;
+
+
+
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user){
+        userService.saveUser(user);
+        return ResponseEntity.ok("User registered Successfuly - \n"+user);
+    }
+
+
+
+    @PostMapping("login")
+    public ResponseEntity<?>
 
 
 
 
 
 
+
+
+
+
+}
+
+
+// User login will be via userName and password , not with email and password
+// TODO: (email will used for password reset kind of things and will be added later on )
+
+@Data
+class AuthenticationRequest{
+    private String userName;
+    private String password;
+}
+
+
+
+class AuthenticationResponse {
+    private final String jwtToken;
+
+    public AuthenticationResponse(String jwtToken){
+        this.jwtToken = jwtToken;
+    }
+
+    public String getJwtToken(){
+        return jwtToken;
+    }
 }
