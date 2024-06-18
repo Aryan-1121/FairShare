3857eebb051548f5e465d12b9fbc50a1c6b4fae4
Aryan <aryanmaurya879@gmail.com>
2024-06-18 08:13:38 +0530

    created empty restcontroller class for auth register and login purpose

---
+++ backend/src/main/java/com/fairshare/controllers/AuthController.java
@@ -0,0 +1,16 @@
+package com.fairshare.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+
+
+
+
+
+
+}
